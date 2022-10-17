474dc0afa1c0a88dcbb1a1c26668afa6b53139f4
mb0294 <mehdibouzghaya@gmail.com>
2022-10-17 12:14:16 +0100

    Chekpoin-Node-js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("<h1>Hello Node!!!!</h1>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>404<h1/>");
+  }
+});
+server.listen(3000, (err) => {
+  err ? console.log(error) : console.log("server is already listening");
+});
